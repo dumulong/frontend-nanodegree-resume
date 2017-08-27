5e9a14f4df410652dba73c16034f32768975dff2
Denis Dumulong <dumulong@sbcglobal.net>
2017-08-27 09:48:38 -0500

    Rearaging the code.  Adding some lorem ipsum, etc.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,10 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 
+/* --------------------------------------------------------------- */
+/* bio data */
+/* --------------------------------------------------------------- */
+
 var bio = {
     name : "Denis Dumulong",
     role : "Web Developer",
@@ -13,50 +17,14 @@ var bio = {
         twitter: "https://twitter.com/TwitIt",
         location: "San Antonio"
     },
-    welcomeMessage: "Hey, there!",
+    welcomeMessage: "Proin convallis justo eget diam ornare suscipit. Sit amet porttitor quam sapien ac lacus.",
     skills: [ "Javascript", "HTML", "CSS", "Other" ],
-    biopic: "http://via.placeholder.com/200x250/FF5733/ffffff?text=My+Photo",
-    display: function () {
-
-        var _headerName = HTMLheaderName.replace("%data%", bio.name);
-        var _headerRole = HTMLheaderRole.replace("%data%", bio.role);
-        var _mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-        var _email = HTMLemail.replace("%data%", bio.contacts.email);
-        var _github = HTMLgithub.replace("%data%", bio.contacts.github);
-        var _twitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-        var _location = HTMLlocation.replace("%data%", bio.contacts.location);
-        var _bioPic = HTMLbioPic.replace("%data%", bio.biopic);
-        var _welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-
-        $("#header").prepend(_headerName);
-        $("#header h1").append(_headerRole);
-
-        var $contact = $("#topContacts");
-        $contact.append(_mobile);
-        $contact.append(_email);
-        $contact.append(_github);
-        $contact.append(_twitter);
-        $contact.append(_location);
-
-        $("#header").append(_bioPic);
-
-        $("#header").append(_welcomeMsg);
-        $("#header").append(HTMLskillsStart);
-
-        bio.skills.forEach(function(elm) {
-            var _skill = HTMLskills.replace("%data%", elm);
-            $("#header").append(_skill);
-        });
-
-        //While we are at it, fill the contact in the bottom:
-        var $contact = $("#footerContacts");
-        $contact.append(_mobile);
-        $contact.append(_email);
-        $contact.append(_github);
-        $contact.append(_twitter);
-        $contact.append(_location);
-    }
+    biopic: "http://via.placeholder.com/200x250/FF5733/ffffff?text=My+Photo"
 };
+
+/* --------------------------------------------------------------- */
+/* work data */
+/* --------------------------------------------------------------- */
 
 var work = {
     jobs: [
@@ -65,72 +33,40 @@ var work = {
             title: "Cook",
             location: "French place",
             dates: "in progress",
-            description: "Cooking food.",
+            description: "Nunc vehicula, lectus sed aliquam lobortis, nisi eros vulputate arcu, sit amet blandit nisi orci nec diam. Praesent at dignissim mauris. Maecenas bibendum velit quis interdum tristique. Donec ut eros lectus. Duis rhoncus quam vel turpis sodales ornare. Quisque ex velit, mollis sed nisl quis, pretium varius sem. Aliquam ornare est sed nisl dapibus molestie. Maecenas mi elit, varius sodales sem vel, venenatis suscipit magna. Donec et faucibus purus, sit amet ultrices lectus. Donec augue erat, porta id metus vel, sodales tincidunt leo. Vivamus pellentesque lacus erat, et egestas nisi interdum ac.",
         },
         {
             employer: "Chez Henrietta",
             title: "Cook",
             location: "Same French place",
             dates: "03/01/2013 - 04/01/2013",
-            description: "Cooking food, but better",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sollicitudin tortor ut iaculis cursus. Maecenas euismod est semper condimentum sodales. Cras eget nisi malesuada, bibendum lorem sit amet, tempus sem. Cras eget purus tincidunt, pellentesque massa et, volutpat nibh. In ornare lorem at ante tincidunt elementum ut ac diam. Proin convallis justo eget diam ornare suscipit. Suspendisse vehicula, odio viverra cursus laoreet, est tortor dignissim odio, sit amet porttitor quam sapien ac lacus.",
         }
-    ],
-    display: function () {
-
-        work.jobs.forEach(function(elm) {
-
-            var _workEmployer = HTMLworkEmployer.replace("%data%", elm.employer);
-            var _workTitle = HTMLworkTitle.replace("%data%", elm.title);
-            var _workDates = HTMLworkDates.replace("%data%", elm.dates);
-            var _workLocation = HTMLworkLocation.replace("%data%", elm.location);
-            var _workDescription = HTMLworkDescription.replace("%data%", elm.description);
-
-            $("#workExperience").append(HTMLworkStart);
-            var $lastEntry = $(".work-entry:last");
-            $lastEntry.append(_workEmployer);
-            $lastEntry.append(_workTitle);
-            $lastEntry.append(_workDates);
-            $lastEntry.append(_workLocation);
-            $lastEntry.append(_workDescription);
-        });
-
-    }
+    ]
 };
+
+/* --------------------------------------------------------------- */
+/* projects data */
+/* --------------------------------------------------------------- */
 
 var projects = {
     projects: [
         {
           title: "How to make stuff up",
           dates: "03/01/2013 - 04/01/2013",
-          description: "Just making stuff up... I did not do anything, really...",
+          description: "Curabitur semper velit sed libero cursus condimentum. Cras bibendum interdum libero non tincidunt. Nam vel fermentum sapien. Nam vitae ipsum fermentum, dapibus eros et, scelerisque lacus. Integer dapibus ut neque a pellentesque. Duis euismod, velit at pharetra tincidunt, tellus nisl finibus erat, nec porta arcu est ut enim. Morbi eu diam non eros consequat semper. Quisque et risus nunc. Nam molestie dui non sem porttitor, ac fringilla orci mollis. Aliquam eget pharetra nibh. Praesent faucibus, mauris non imperdiet blandit, dui nunc dignissim enim, non molestie tortor dolor at enim. Phasellus hendrerit venenatis sapien, scelerisque porta magna dictum vitae. Donec dignissim, enim et vestibulum egestas, diam dolor dignissim nisl, hendrerit sagittis metus diam sed nunc.",
           images: [
-            "http://via.placeholder.com/350x300/3396FF/000?text=Some+Pic",
-            "http://via.placeholder.com/200x300/3396FF/000?text=Some+Other+Pic",
-            "http://via.placeholder.com/350x300/3396FF/fff?text=Last+Pic"
+            "http://via.placeholder.com/350x300/f5ae23/000?text=Some+Pic",
+            "http://via.placeholder.com/200x300/f5ae23/000?text=Some+Other+Pic",
+            "http://via.placeholder.com/350x300/f5ae23/fff?text=Last+Pic"
             ]
         }
-    ],
-    display: function () {
-
-        projects.projects.forEach(function(elm) {
-
-            var _projectTitle = HTMLprojectTitle.replace("%data%", elm.title);
-            var _projectDates = HTMLprojectDates.replace("%data%", elm.dates);
-            var _projectDescription = HTMLprojectDescription.replace("%data%", elm.description);
-            var _projectImage = HTMLprojectImage.replace("%data%", elm.mobile);
-
-            $("#projects").append(HTMLprojectStart);
-            var $lastEntry = $(".project-entry:last");
-            $lastEntry.append(_projectTitle);
-            $lastEntry.append(_projectDates);
-            $lastEntry.append(_projectDescription);
-            elm.images.forEach(function(pic) {
-                var _pic = HTMLprojectImage.replace("%data%", pic);
-                $lastEntry.append(_pic);
-            });
-        });
-    }
+    ]
 };
+
+/* --------------------------------------------------------------- */
+/* education data */
+/* --------------------------------------------------------------- */
 
 var education = {
 
@@ -165,45 +101,146 @@ var education = {
             dates: "03/01/2013 - 04/01/2013",
             url: "http://fraudschool.com/nothardatall"
         },
-    ],
-    display: function () {
-
-        education.schools.forEach(function(elm) {
-
-            var _schoolName = HTMLschoolName.replace("%data%", elm.name);
-            _schoolName = _schoolName.replace("#", elm.url);
-            var _schoolDegree = HTMLschoolDegree.replace("%data%", elm.degree);
-            var _schoolDates = HTMLschoolDates.replace("%data%", elm.dates);
-            var _schoolLocation = HTMLschoolLocation.replace("%data%", elm.location);
-            var _schoolMajor = HTMLschoolMajor.replace("%data%", elm.majors);
-
-            $("#education").append(HTMLschoolStart);
-            var $lastEntry = $(".education-entry:last");
-            $lastEntry.append(_schoolName);
-            $lastEntry.append(_schoolDegree);
-            $lastEntry.append(_schoolDates);
-            $lastEntry.append(_schoolLocation);
-            $lastEntry.append(_schoolMajor);
-
-        });
-
-        $("#education").append(HTMLonlineClasses);
-
-        education.onlineCourses.forEach(function(elm) {
-
-            var _onlineTitle = HTMLonlineTitle.replace("%data%", elm.title);
-            var _onlineSchool = HTMLonlineSchool.replace("%data%", elm.school);
-            var _onlineDates = HTMLonlineDates.replace("%data%", elm.dates);
-            var _onlineURL = HTMLonlineURL.replace("%data%", elm.url);
-
-            $("#education").append(HTMLschoolStart);
-            var $lastEntry = $(".education-entry:last");
-            $lastEntry.append(_onlineTitle);
-            $lastEntry.append(_onlineSchool);
-            $lastEntry.append(_onlineDates);
-            $lastEntry.append(_onlineURL);
-
-        });
-    }
+    ]
 
 };
+
+
+/* --------------------------------------------------------------- */
+/* bio display */
+/* --------------------------------------------------------------- */
+
+bio.display = function () {
+
+    var _headerName = HTMLheaderName.replace("%data%", bio.name);
+    var _headerRole = HTMLheaderRole.replace("%data%", bio.role);
+    var _mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var _email = HTMLemail.replace("%data%", bio.contacts.email);
+    var _github = HTMLgithub.replace("%data%", bio.contacts.github);
+    var _twitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    var _location = HTMLlocation.replace("%data%", bio.contacts.location);
+    var _bioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var _welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+    $("#header").prepend(_headerName);
+    $("#header h1").append(_headerRole);
+
+    var $contact = $("#topContacts");
+    $contact.append(_mobile);
+    $contact.append(_email);
+    $contact.append(_github);
+    $contact.append(_twitter);
+    $contact.append(_location);
+
+    $("#header").append(_bioPic);
+
+    $("#header").append(_welcomeMsg);
+    $("#header").append(HTMLskillsStart);
+
+    this.skills.forEach(function(elm) {
+        var _skill = HTMLskills.replace("%data%", elm);
+        $("#header").append(_skill);
+    });
+
+    //While we are at it, fill the contact in the bottom:
+    var $contact = $("#footerContacts");
+    $contact.append(_mobile);
+    $contact.append(_email);
+    $contact.append(_github);
+    $contact.append(_twitter);
+    $contact.append(_location);
+}
+
+/* --------------------------------------------------------------- */
+/* work display */
+/* --------------------------------------------------------------- */
+
+work.display = function () {
+
+    this.jobs.forEach(function(elm) {
+
+        var _workEmployer = HTMLworkEmployer.replace("%data%", elm.employer);
+        var _workTitle = HTMLworkTitle.replace("%data%", elm.title);
+        var _workDates = HTMLworkDates.replace("%data%", elm.dates);
+        var _workLocation = HTMLworkLocation.replace("%data%", elm.location);
+        var _workDescription = HTMLworkDescription.replace("%data%", elm.description);
+
+        $("#workExperience").append(HTMLworkStart);
+        var $lastEntry = $(".work-entry:last");
+        $lastEntry.append(_workEmployer);
+        $lastEntry.append(_workTitle);
+        $lastEntry.append(_workDates);
+        $lastEntry.append(_workLocation);
+        $lastEntry.append(_workDescription);
+    });
+
+}
+
+/* --------------------------------------------------------------- */
+/* projects display */
+/* --------------------------------------------------------------- */
+
+projects.display = function () {
+
+    this.projects.forEach(function(elm) {
+
+        var _projectTitle = HTMLprojectTitle.replace("%data%", elm.title);
+        var _projectDates = HTMLprojectDates.replace("%data%", elm.dates);
+        var _projectDescription = HTMLprojectDescription.replace("%data%", elm.description);
+        var _projectImage = HTMLprojectImage.replace("%data%", elm.mobile);
+
+        $("#projects").append(HTMLprojectStart);
+        var $lastEntry = $(".project-entry:last");
+        $lastEntry.append(_projectTitle);
+        $lastEntry.append(_projectDates);
+        $lastEntry.append(_projectDescription);
+        elm.images.forEach(function(pic) {
+            var _pic = HTMLprojectImage.replace("%data%", pic);
+            $lastEntry.append(_pic);
+        });
+    });
+}
+
+/* --------------------------------------------------------------- */
+/* eduction display */
+/* --------------------------------------------------------------- */
+
+education.display = function () {
+
+    this.schools.forEach(function(elm) {
+
+        var _schoolName = HTMLschoolName.replace("%data%", elm.name);
+        _schoolName = _schoolName.replace("#", elm.url);
+        var _schoolDegree = HTMLschoolDegree.replace("%data%", elm.degree);
+        var _schoolDates = HTMLschoolDates.replace("%data%", elm.dates);
+        var _schoolLocation = HTMLschoolLocation.replace("%data%", elm.location);
+        var _schoolMajor = HTMLschoolMajor.replace("%data%", elm.majors);
+
+        $("#education").append(HTMLschoolStart);
+        var $lastEntry = $(".education-entry:last");
+        $lastEntry.append(_schoolName);
+        $lastEntry.append(_schoolDegree);
+        $lastEntry.append(_schoolDates);
+        $lastEntry.append(_schoolLocation);
+        $lastEntry.append(_schoolMajor);
+
+    });
+
+    $("#education").append(HTMLonlineClasses);
+
+    this.onlineCourses.forEach(function(elm) {
+
+        var _onlineTitle = HTMLonlineTitle.replace("%data%", elm.title);
+        var _onlineSchool = HTMLonlineSchool.replace("%data%", elm.school);
+        var _onlineDates = HTMLonlineDates.replace("%data%", elm.dates);
+        var _onlineURL = HTMLonlineURL.replace("%data%", elm.url);
+
+        $("#education").append(HTMLschoolStart);
+        var $lastEntry = $(".education-entry:last");
+        $lastEntry.append(_onlineTitle);
+        $lastEntry.append(_onlineSchool);
+        $lastEntry.append(_onlineDates);
+        $lastEntry.append(_onlineURL);
+
+    });
+}
