e3b91c8aa1db9d4450643123a0392a76665ee9ec
Denis Dumulong <dumulong@sbcglobal.net>
2017-08-26 22:20:25 -0500

    zzz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,7 +4,7 @@ This is empty on purpose! Your code to build the resume will go here.
 
 
 var bio = {
-    name : "Denis DUmulong",
+    name : "Denis Dumulong",
     role : "Web Developer",
     contacts : {
         mobile: "(210) 867-5309",
@@ -15,9 +15,118 @@ var bio = {
     },
     welcomeMessage: "Hey, there!",
     skills: [ "Javascript", "HTML", "CSS", "Other" ],
-    biopic: "http://",
-    display: function () { }
-}
+    biopic: "http://via.placeholder.com/350x300",
+    display: function () {
+
+        var _headerName = HTMLheaderName.replace("%data%", bio.name);
+        var _headerRole = HTMLheaderRole.replace("%data%", bio.role);
+        var _mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var _email = HTMLemail.replace("%data%", bio.contacts.email);
+        var _github = HTMLgithub.replace("%data%", bio.contacts.github);
+        var _twitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var _location = HTMLlocation.replace("%data%", bio.contacts.location);
+        var _bioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        var _welcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        $("#header").prepend(_headerName);
+        $("#header h1").append(_headerRole);
+
+        var $contact = $("#topContacts");
+        $contact.append(_mobile);
+        $contact.append(_email);
+        $contact.append(_github);
+        $contact.append(_twitter);
+        $contact.append(_location);
+
+        $("#header").append(_bioPic);
+
+        $("#header").append(_welcomeMsg);
+        $("#header").append(HTMLskillsStart);
+
+        bio.skills.forEach(function(elm) {
+            var _skill = HTMLskills.replace("%data%", elm);
+            $("#header").append(_skill);
+        });
+
+        //While we are at it, fill the contact in the bottom:
+        var $contact = $("#footerContacts");
+        $contact.append(_mobile);
+        $contact.append(_email);
+        $contact.append(_github);
+        $contact.append(_twitter);
+        $contact.append(_location);
+    }
+};
+
+var work = {
+    jobs: [
+        {
+            employer: "Chez Henry",
+            title: "Cook",
+            location: "French place",
+            dates: "in progress",
+            description: "Cooking food.",
+        },
+        {
+            employer: "Chez Henrietta",
+            title: "Cook",
+            location: "Same French place",
+            dates: "03/01/2013 - 04/01/2013",
+            description: "Cooking food, but better",
+        }
+    ],
+    display: function () {
+
+        work.jobs.forEach(function(elm) {
+
+            var _workEmployer = HTMLworkEmployer.replace("%data%", elm.employer);
+            var _workTitle = HTMLworkTitle.replace("%data%", elm.title);
+            var _workDates = HTMLworkDates.replace("%data%", elm.dates);
+            var _workLocation = HTMLworkLocation.replace("%data%", elm.location);
+            var _workDescription = HTMLworkDescription.replace("%data%", elm.description);
+
+            $("#workExperience").append(HTMLworkStart);
+            var $lastEntry = $(".work-entry:last");
+            $lastEntry.append(_workEmployer);
+            $lastEntry.append(_workTitle);
+            $lastEntry.append(_workDates);
+            $lastEntry.append(_workLocation);
+            $lastEntry.append(_workDescription);
+        });
+
+    }
+};
+
+var projects = {
+    projects: [
+        {
+          title: "How to make stuff up",
+          dates: "03/01/2013 - 04/01/2013",
+          description: "Just making stuff up... I did not do anything, really...",
+          images: [ "http://via.placeholder.com/350x300", "http://via.placeholder.com/350x300", "http://via.placeholder.com/350x300" ]
+        }
+    ],
+    display: function () {
+
+        projects.projects.forEach(function(elm) {
+
+            var _projectTitle = HTMLprojectTitle.replace("%data%", elm.title);
+            var _projectDates = HTMLprojectDates.replace("%data%", elm.dates);
+            var _projectDescription = HTMLprojectDescription.replace("%data%", elm.description);
+            var _projectImage = HTMLprojectImage.replace("%data%", elm.mobile);
+
+            $("#projects").append(HTMLprojectStart);
+            var $lastEntry = $(".project-entry:last");
+            $lastEntry.append(_projectTitle);
+            $lastEntry.append(_projectDates);
+            $lastEntry.append(_projectDescription);
+            elm.images.forEach(function(pic) {
+                var _pic = HTMLprojectImage.replace("%data%", pic);
+                $lastEntry.append(_pic);
+            });
+        });
+    }
+};
 
 var education = {
 
@@ -25,7 +134,7 @@ var education = {
         {
             name: "Some Elementary School",
             location: "NoWhere Near",
-            degree: "Sure",
+            degree: "BA",
             majors: "Pain",
             dates: "01/01/2001 - 02/01/2001",
             url: "http://fakefreakingschool.com/NoWhere"
@@ -33,7 +142,7 @@ var education = {
         {
             name: "Some High School",
             location: "Closer then the Other",
-            degree: "Bof...",
+            degree: "Master",
             majors: "More like Kernel",
             dates: "03/01/2001 - 04/01/2001",
             url: "http://fakefreakingschool.com/Closer"
@@ -53,38 +162,44 @@ var education = {
             url: "http://fraudschool.com/nothardatall"
         },
     ],
-    display: function () { }
+    display: function () {
 
-}
+        education.schools.forEach(function(elm) {
 
-var work = {
-    jobs: [
-        {
-            employer: "Chez Henry",
-            title: "Cook",
-            location: "French place",
-            dates: "in progress",
-            description: "Cooking food.",
-        },
-        {
-            employer: "Chez Henrietta",
-            title: "Cook",
-            location: "Same French place",
-            dates: "03/01/2013 - 04/01/2013",
-            description: "Cooking food, but better",
-        }
-    ],
-    display: function () { }
-}
+            var _schoolName = HTMLschoolName.replace("%data%", elm.name);
+            _schoolName = _schoolName.replace("#", elm.url);
+            var _schoolDegree = HTMLschoolDegree.replace("%data%", elm.degree);
+            var _schoolDates = HTMLschoolDates.replace("%data%", elm.dates);
+            var _schoolLocation = HTMLschoolLocation.replace("%data%", elm.location);
+            var _schoolMajor = HTMLschoolMajor.replace("%data%", elm.majors);
 
-var projects = {
-    projects: [
-        {
-          title: "How to make stuff up",
-          dates: "03/01/2013 - 04/01/2013",
-          description: "Just making stuff up... I did not do anything, really...",
-          images: [ "http://", "", "" ]
-        }
-    ],
-    display: function () { }
-}
+            $("#education").append(HTMLschoolStart);
+            var $lastEntry = $(".education-entry:last");
+            $lastEntry.append(_schoolName);
+            $lastEntry.append(_schoolDegree);
+            $lastEntry.append(_schoolDates);
+            $lastEntry.append(_schoolLocation);
+            $lastEntry.append(_schoolMajor);
+
+        });
+
+        $("#education").append(HTMLonlineClasses);
+
+        education.onlineCourses.forEach(function(elm) {
+
+            var _onlineTitle = HTMLonlineTitle.replace("%data%", elm.title);
+            var _onlineSchool = HTMLonlineSchool.replace("%data%", elm.school);
+            var _onlineDates = HTMLonlineDates.replace("%data%", elm.dates);
+            var _onlineURL = HTMLonlineURL.replace("%data%", elm.url);
+
+            $("#education").append(HTMLschoolStart);
+            var $lastEntry = $(".education-entry:last");
+            $lastEntry.append(_onlineTitle);
+            $lastEntry.append(_onlineSchool);
+            $lastEntry.append(_onlineDates);
+            $lastEntry.append(_onlineURL);
+
+        });
+    }
+
+};
